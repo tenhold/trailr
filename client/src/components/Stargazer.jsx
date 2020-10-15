3bec984b77344ccf5bebbba81e8a8b45d498e9c5
Scott Anderson <scott.anderson1130@gmail.com>
2020-10-15 12:05:48 -0500

    [Stargazer] created page and started routes

---
+++ client/src/components/Stargazer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from "./header"
+
+const Stargazer = () => {
+  
+
+return (
+    <Container>
+
+     <h1>Welcome to the Stargazer Page!!!</h1>
+    </Container>
+)}
+
+
+
+export default Stargazer;
