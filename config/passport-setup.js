e09a2da91bb5e74170a51ebbc00113af4e213d43
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-10-14 20:17:27 -0500

    [Bug] Work on Oauth bug.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -9,28 +9,35 @@ const { addUser } = require('../database/index');
 
 // set up passport middleware to use google strategy in our project
 passport.use(
-  new GoogleStrategy({
-    // options for the strategy, input clientID && clientSecret
-    callbackURL: process.env.NODE_ENV === 'PROD' ? 'https://trailr.org/auth/google/redirect' : '/auth/google/redirect',
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }, (accessToken, refreshToken, profile, done) => {
-    // passport callback function
-    // console.log('passport callback fired'); // indication that function fired
-    console.log('USER PROFILE', profile); // shows returned profile information
-    const { displayName, id, photos } = profile;
-    addUser({
-      google_id: id,
-      name: displayName,
-      profile_photo_url: photos[0].value,
-    })
-      .then((newUser) => {
-        console.log(`Created New User: ${newUser}`);
-        // completes the http request, and sends information to next function in middleware chain
-        done(null, newUser);
+  new GoogleStrategy(
+    {
+      // options for the strategy, input clientID && clientSecret
+      callbackURL:
+        process.env.NODE_ENV === 'PROD'
+          ? 'https://trailr.org/auth/google/redirect'
+          : '/auth/google/redirect',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // passport callback function
+      // console.log('passport callback fired'); // indication that function fired
+      console.log('USER PROFILE', profile); // shows returned profile information
+      console.log(process.env.GOOGLE_CLIENT_ID);
+      const { displayName, id, photos } = profile;
+      addUser({
+        google_id: id,
+        name: displayName,
+        profile_photo_url: photos[0].value,
       })
-      .catch((error) => {
-        done(error);
-      });
-  }),
+        .then((newUser) => {
+          console.log(`Created New User: ${newUser}`);
+          // completes the http request, and sends information to next function in middleware chain
+          done(null, newUser);
+        })
+        .catch((error) => {
+          done(error);
+        });
+    }
+  )
 );
